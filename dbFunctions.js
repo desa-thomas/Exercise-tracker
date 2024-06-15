6c008665d7d1f14f0a1fe5d28bcb6c4ec37ba560
desa-thomas <desa2thomas@gmail.com>
2024-06-15 01:01:15 -0400

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	modified:   views/index.html

---
+++ dbFunctions.js
@@ -0,0 +1,152 @@
+const { Model } = require("mongoose");
+
+/**
+ *
+ * @param {Model} User - User model
+ * @param {String} username - Username
+ * @param {Callback} done - Call back funciton
+ */
+function createUser(User, username, done) {
+  let user = new User({ username: username });
+
+  user.save(function (err, document) {
+    if (err) return console.error(err);
+
+    console.log("User Saved:", document);
+    done(null, document);
+  });
+}
+
+/**
+ * Creates exercise document and saves it to database
+ * @param {Model} User
+ * @param {Model} Exercise
+ * @param {String} id
+ * @param {String} description
+ * @param {Number} duration
+ * @param {Date} date -optional
+ * @param {Callback} done
+ */
+function createExercise(
+  User,
+  Exercise,
+  id,
+  description,
+  duration,
+  date,
+  done
+) {
+  //if date field is null, set it to current date
+  let exercise;
+
+  User.findById(id, function (err, user) {
+    if (err) {
+      done(err, null, null);
+      return console.error(err);
+    }
+
+    //if user exists in db
+    if (user) {
+      //if date was given
+      if (date) {
+        exercise = new Exercise({
+          user_id: id,
+          description: description,
+          duration: duration,
+          date: date,
+        });
+      } else {
+        exercise = new Exercise({
+          user_id: id,
+          description: description,
+          duration: duration,
+        });
+      }
+
+      exercise.save(function (err, exercise) {
+        if (err) {
+          done(err, null, null);
+          return console.error(err);
+        }
+
+        console.log("exercise created", exercise);
+        done(null, user, exercise);
+      });
+    } 
+    //If user does not exist 
+    else {
+      console.log("User does not exist");
+      done("User does not exist", null,null);
+    }
+  });
+}
+/**
+ *
+ * @param {Model} User - User model
+ * @param {Callback} done - callback function
+ */
+function getAllUsers(User, done) {
+  User.find(function (err, users) {
+    if (err) return console.error(err);
+
+    done(null, users);
+  });
+}
+
+/**
+ * Get the exercise log of a user given their ID 
+ * @param {Model} User 
+ * @param {Model} Exercise 
+ * @param {*} id 
+ * @param {*} done 
+ */
+function getLog(User, Exercise, id, queryParam, done){
+  User.findById(id, function(err, user){
+    if(err){
+      done(err, null, null)
+      return console.error(err)
+    }
+
+    if(!user){
+      console.log("User does not exist")
+      done("User does not exist", null, null)
+    }
+
+    //If user is found 
+    else{
+      //If no query parameters (from, to, limit)
+
+        let query = Exercise.find({user_id: id})
+        .select({_id: 0, user_id: 0, __v: 0})
+
+        if(queryParam){
+          if(queryParam.from){
+            query.where("date").gte(queryParam.from)
+          }
+          if(queryParam.to){
+            query.where("date").lte(queryParam.to)
+          }
+          if(queryParam.limit){
+            query.limit(Number(queryParam.limit))
+          }
+        }
+        query.exec(function(err, log){
+          if(err){
+            done(err, null, null)
+            return console.error(err)
+          }
+          //else return log and user
+          done(null, user, log)
+        })
+     
+    }
+
+  })
+}
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  createExercise,
+  getLog
+};
--- index.js
+++ index.js
@@ -1,18 +1,114 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+const mongoose = require("mongoose");
+const dbfunc = require("./dbFunctions");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  userUnifiedTopology: true,
 });
 
+let userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+});
 
+let exerciseSchema = new mongoose.Schema({
+  user_id: { required: true, type: String },
+  description: { required: true, type: String },
+  duration: { required: true, type: Number },
+  date: { type: Date, default: Date.now },
+});
 
+//Creating User and Exercise models
+let User = new mongoose.model("User", userSchema, "User");
+let Exercise = new mongoose.model("Exercise", exerciseSchema, "Exercise");
 
+//Route handler for /api/users
+app
+  .route("/api/users")
+  .post(function (req, res) {
+    dbfunc.createUser(User, req.body.username, function (err, document) {
+      if (err) return console.error(err);
+
+      res.json({ username: document.username, _id: document._id });
+    });
+  })
+  .get(function (req, res) {
+    dbfunc.getAllUsers(User, function (err, userList) {
+      if (err) return console.error(err);
+
+      res.send(userList);
+    });
+  });
+
+app.route("/api/users/:_id/exercises").post(function (req, res) {
+  dbfunc.createExercise(
+    User,
+    Exercise,
+    req.params._id,
+    req.body.description,
+    req.body.duration,
+    req.body.date,
+
+    function (err, user, exercise) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({
+          _id: user._id,
+          username: user.username,
+          date: exercise.date.toDateString(),
+          duration: exercise.duration,
+          description: exercise.description,
+        });
+      }
+    }
+  );
+});
+
+//?[from][&to][&limit]
+app.get("/api/users/:_id/logs", function (req, res) {
+  let query;
+  if (Object.keys(req.query).length != 0) {
+    query = req.query;
+  } else {
+    query = null;
+  }
+  dbfunc.getLog(
+    User,
+    Exercise,
+    req.params._id,
+    query,
+    function (err, user, log) {
+      if (err) {
+        res.send(err);
+      } else {
+        let count = log.length;
+        res.json({
+          username: user.username,
+          count: count,
+          _id: user._id,
+          log: log,
+        });
+      }
+    }
+  );
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
